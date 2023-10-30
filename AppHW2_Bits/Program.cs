d5179d6afb75bf92b2ba5e58f66aa60f8414ac13
kvo52@kvo1 <kvo.52@mail.ru>
2023-10-30 18:57:53 +0300

    AppHW_02_Bits

---
+++ AppHW2_Bits/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using AppHW2_Bits;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var value = new Bits(7);
+
+        var a = "";
+        try
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                a += value.GetBit(i) ? 1 : 0;
+            }
+            a = string.Join("", a.Reverse());
+
+            Console.WriteLine(a);
+            Console.WriteLine(value.GetBit(5));
+
+            value.SetBit(false, 2);
+            Console.WriteLine(value.Value);
+
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+         }
+    }
+}
+
