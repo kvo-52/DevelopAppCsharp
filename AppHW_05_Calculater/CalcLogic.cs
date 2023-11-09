352c7280d69f96c8564e0c79ccfffa9a4a36d6d8
kvo52@kvo1 <kvo.52@mail.ru>
2023-11-09 19:39:58 +0300

    HW_05_Calculater

---
+++ AppHW_05_Calculater/CalcLogic.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppHW_05_Calculater
+{
+    internal class CalcLogic : ICalc
+    {
+        public double Result { get; set; } = 0D;
+
+        public event EventHandler<EventArgs> MyEventHandler;
+
+        private Stack<double> LastResult { get; set; } = new Stack<double>();
+
+        public void PrintResult() {
+            MyEventHandler?.Invoke(this, new EventArgs());
+        }
+        public void Divide(int x)
+        {
+            Result/=x;
+            PrintResult();
+            LastResult.Push(Result);
+
+        }
+
+        public void Multy(int x)
+        {
+            Result*=x;
+            PrintResult();
+            LastResult.Push(Result);
+        }
+
+        public void Sub(int x)
+        {
+            Result -=x;
+            PrintResult();
+            LastResult.Push(Result);
+        }
+
+        public void Sum(int x)
+        {
+            Result += x;
+            PrintResult();
+            LastResult.Push(Result);
+
+        }
+
+        public void CancleLast()
+        {
+           if( LastResult.TryPop(out double res))
+            {
+                Result = res;
+                Console.WriteLine("Последнее действие отменено. Результат =");
+                PrintResult();
+            }
+            else
+            {
+                Console.WriteLine("Невозможно отменить последнее действие.");
+            }
+
+        }
+
+
+    }
+}
---
+++ AppHW_05_Calculater_2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppHW_05_Calculater
+{
+    internal class Program
+    {
+        static void Main()
+
+        {
+
+            string action = "";
+
+            do
+            {
+
+                {
+
+                    Console.WriteLine("Выберите действие: +, -, *, /, q (для выхода)");
+
+                    action = Console.ReadLine();
+
+
+
+                    if (action == "+" || action == "-" || action == "*" || action == "/")
+
+                    {
+
+                        Console.WriteLine("Введите первое число:");
+
+                        double num1 = Convert.ToDouble(Console.ReadLine());
+
+
+
+                        Console.WriteLine("Введите второе число:");
+
+                        double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+                        double result = 0;
+
+
+
+                        switch (action)
+
+                        {
+
+                            case "+":
+
+                                result = num1 + num2;
+
+                                break;
+
+                            case "-":
+
+                                result = num1 - num2;
+
+                                break;
+
+                            case "*":
+
+                                result = num1 * num2;
+
+                                break;
+
+                            case "/":
+
+                                result = num1 / num2;
+
+                                break;
+
+                            default:
+
+                                Console.WriteLine("Некорректное действие!");
+
+                                break;
+
+                        }
+
+
+
+                        Console.WriteLine("Результат: " + result);
+
+                    }
+
+                    else if (action != "q" && action != "")
+
+                    {
+
+                        Console.WriteLine("Некорректное действие!");
+
+                    }
+                }
+
+            } while (action != "q" && action != "");
+            Console.WriteLine("Программа завершена");
+        }
+    }
+
+}
