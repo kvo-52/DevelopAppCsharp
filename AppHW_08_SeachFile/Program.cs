1f49907ddc2bdcbd98841908c45b9f419162cd00
kvo52@kvo1 <kvo.52@mail.ru>
2023-11-28 15:32:23 +0300

    HW_08_seashfile

---
+++ AppHW_08_SeachFile/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace AppHW_08_SeachFile
+{
+
+    //поиск файла и поиск текста в файле написав утилиту, которая ищет файлы определенного расширения с указанным текстом. Рекурсивно.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = "D:\\GeekBrains\\БЛОК-3_Developer\\Developer_C#\\2.Csharp_Application\\AppHomeWork\\AppHW_08_SeachFile"; //Путь_к_директории
+            string fileName = "testHW08.txt"; //Имя_файла
+            string searchText = "hello world"; //поиск текста
+
+            UtilitySeachFile.SearchForFile(directory, fileName); // поиск файла
+            UtilitySeachFile.SearchForTextInFile(fileName, searchText); //поиск текста
+        }
+
+    }
+}
---
+++ AppHW_08_SeachFile/UtilitySeachFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppHW_08_SeachFile
+{
+    internal class UtilitySeachFile
+    {
+        public static void SearchForFile(string directory, string fileName)
+        {
+            var files = Directory.EnumerateFiles(directory, fileName, SearchOption.AllDirectories);
+            foreach (var path in files)
+            {
+                Console.WriteLine($"Искомый файл {fileName} находится по пути: {path}");
+            }
+           
+        }
+
+        public static void SearchForTextInFile(string fileName, string searchText)
+        {
+            if (File.Exists(fileName))
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.Contains(searchText))
+                        {
+                            Console.WriteLine($"Найдено значение {searchText} в файле {fileName}: {line}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл не существует");
+            }
+        }
+    }
+}
